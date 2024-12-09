18b8f362dddb485e8ad55e659ddfcc18c0a9b09f
meetprolanceit <meetproexlancer@gmail.com>
2024-12-09 12:15:58 +0530

    add stash code sum function in index.js

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ console.log('No i want to show my contact details can you show me that?');
 console.log(
     'hello guys i am form about branch and i do want to commite the one commite for after i join the main barnch'
 );
+
+const sum = (a,b) => {
+    return a + b;
+};
+
+sum()
